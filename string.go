eb8e28abbfc8d758c16595505559b25786c0cb92
LinjieDong <linjiedong@linkdoc.com>
2019-07-22 16:25:09 +0800

    v2

--- string.go
+++ string.go
@@ -2,6 +2,15 @@ package stringsx
 
 import "fmt"
 
-func Hello(name string) (string,error){
-	return fmt.Sprintf("Hello ，%s",name),nil
+func Hello(name,lang string) (string,error){
+	switch lang{
+	case "en":
+		return fmt.Sprintf("Hello ，%s!",name),nil
+	case "zh":
+		return fmt.Sprintf("你好，%s!",name ) , nil
+	case "fr":
+		return fmt.Sprintf("Bonjour,%s!",name) , nil
+	default:
+		return "", fmt.Errorf("unkown language")
+	}
 }
